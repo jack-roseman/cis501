b500e460d7037e9753e188b2991f6a8ef7084b29
Jack Roseman <jackroseman33@yahoo.com>
2020-05-12 11:07:56 -0400

    saving changes

--- lab5-regfile/lc4_regfile_ss.v
+++ lab5-regfile/lc4_regfile_ss.v
@@ -39,6 +39,49 @@ module lc4_regfile_ss #(parameter n = 16)
     input  wire          i_rd_we_B   // pipe B: write enable
     );
 
-   /*** TODO: Your Code Here ***/
+   wire should_write_A = ~(i_rd_A == i_rd_B);
+   wire should_write_B = (i_rd_A == i_rd_B);
 
+   
+
+   lc4_regfile c1(.clk(clk), .gwe(gwe), .rst(rst), 
+                  .i_rd(i_rd_A), .i_rd_we(i_rd_we_A), .i_wdata(i_wdata_A),
+                  .i_rs(i_rs_A), .i_rt(i_rt_A),
+                  .o_rs_data(o_rs_data_A), .o_rt_data(o_rt_data_A));
+   
+   lc4_regfile c2(.clk(clk), .gwe(gwe), .rst(rst), 
+                  .i_rd(i_rd_B), .i_rd_we(i_rd_we_B), .i_wdata(i_wdata_B),
+                  .i_rs(i_rs_B), .i_rt(i_rt_B),
+                  .o_rs_data(o_rs_data_B), .o_rt_data(o_rt_data_B));
+endmodule
+
+
+
+module lc4_regfile #(parameter n = 16)
+   (input  wire         clk,
+    input  wire         gwe,
+    input  wire         rst,
+    input  wire [  2:0] i_rs,      // rs selector
+    output wire [n-1:0] o_rs_data, // rs contents
+    input  wire [  2:0] i_rt,      // rt selector
+    output wire [n-1:0] o_rt_data, // rt contents
+    input  wire [  2:0] i_rd,      // rd selector
+    input  wire [n-1:0] i_wdata,   // data to write
+    input  wire         i_rd_we    // write enable
+    );
+
+   wire [n-1:0] r0, r1, r2, r3, r4, r5, r6, r7;
+   
+   Nbit_reg #(n) r_0(.in(i_wdata), .clk(clk), .we(i_rd == 3'b000 & i_rd_we), .gwe(gwe), .rst(rst), .out(r0));
+   Nbit_reg #(n) r_1(.in(i_wdata), .clk(clk), .we(i_rd == 3'b001 & i_rd_we), .gwe(gwe), .rst(rst), .out(r1));
+   Nbit_reg #(n) r_2(.in(i_wdata), .clk(clk), .we(i_rd == 3'b010 & i_rd_we), .gwe(gwe), .rst(rst), .out(r2));
+   Nbit_reg #(n) r_3(.in(i_wdata), .clk(clk), .we(i_rd == 3'b011 & i_rd_we), .gwe(gwe), .rst(rst), .out(r3));
+   Nbit_reg #(n) r_4(.in(i_wdata), .clk(clk), .we(i_rd == 3'b100 & i_rd_we), .gwe(gwe), .rst(rst), .out(r4));
+   Nbit_reg #(n) r_5(.in(i_wdata), .clk(clk), .we(i_rd == 3'b101 & i_rd_we), .gwe(gwe), .rst(rst), .out(r5));
+   Nbit_reg #(n) r_6(.in(i_wdata), .clk(clk), .we(i_rd == 3'b110 & i_rd_we), .gwe(gwe), .rst(rst), .out(r6));
+   Nbit_reg #(n) r_7(.in(i_wdata), .clk(clk), .we(i_rd == 3'b111 & i_rd_we), .gwe(gwe), .rst(rst), .out(r7));
+   
+   assign o_rs_data = i_rs == 3'b000 ? r0 : (i_rs == 3'b001 ? r1 :  (i_rs == 3'b010 ? r2 : (i_rs == 3'b011 ? r3 : (i_rs == 3'b100 ? r4 : (i_rs == 3'b101 ? r5 : (i_rs == 3'b110 ? r6 : r7))))));
+   assign o_rt_data = i_rt == 3'b000 ? r0 : (i_rt == 3'b001 ? r1 :  (i_rt == 3'b010 ? r2 : (i_rt == 3'b011 ? r3 : (i_rt == 3'b100 ? r4 : (i_rt == 3'b101 ? r5 : (i_rt == 3'b110 ? r6 : r7))))));
+   
 endmodule
--- lab5-superscalar/lc4_regfile_ss.v
+++ lab5-superscalar/lc4_regfile_ss.v
@@ -39,20 +39,25 @@ module lc4_regfile_ss #(parameter n = 16)
     input  wire          i_rd_we_B   // pipe B: write enable
     );
 
-   /*** TODO: Your Code Here ***/
+   wire [2:0] rd;
    wire [n-1:0] r0, r1, r2, r3, r4, r5, r6, r7;
+   wire [n-1:0] data0, data1, data2, data3, data4, data5, data6, data7;
+   assign data0 = i_rd_B == 3'b000 && (i_rd_A == i_rd_B ? (i_rd_B == 3'b000 ? i_wdata_B) : i_wdata_A
    
-   Nbit_reg #(n) r_0(.in(i_wdata), .clk(clk), .we(i_rd == 3'b000 & i_rd_we), .gwe(gwe), .rst(rst), .out(r0));
-   Nbit_reg #(n) r_1(.in(i_wdata), .clk(clk), .we(i_rd == 3'b001 & i_rd_we), .gwe(gwe), .rst(rst), .out(r1));
-   Nbit_reg #(n) r_2(.in(i_wdata), .clk(clk), .we(i_rd == 3'b010 & i_rd_we), .gwe(gwe), .rst(rst), .out(r2));
-   Nbit_reg #(n) r_3(.in(i_wdata), .clk(clk), .we(i_rd == 3'b011 & i_rd_we), .gwe(gwe), .rst(rst), .out(r3));
-   Nbit_reg #(n) r_4(.in(i_wdata), .clk(clk), .we(i_rd == 3'b100 & i_rd_we), .gwe(gwe), .rst(rst), .out(r4));
-   Nbit_reg #(n) r_5(.in(i_wdata), .clk(clk), .we(i_rd == 3'b101 & i_rd_we), .gwe(gwe), .rst(rst), .out(r5));
-   Nbit_reg #(n) r_6(.in(i_wdata), .clk(clk), .we(i_rd == 3'b110 & i_rd_we), .gwe(gwe), .rst(rst), .out(r6));
-   Nbit_reg #(n) r_7(.in(i_wdata), .clk(clk), .we(i_rd == 3'b111 & i_rd_we), .gwe(gwe), .rst(rst), .out(r7));
+   Nbit_reg #(n) r_0(.in(data0), .clk(clk), .we((i_rd_A == 3'b000 || i_rd_B == 3'b000) & (i_rd_we_A || i_rd_we_A)), .gwe(gwe), .rst(rst), .out(r0));
+   Nbit_reg #(n) r_1(.in(data1), .clk(clk), .we((i_rd_A == 3'b001 || i_rd_B == 3'b001) & (i_rd_we_A || i_rd_we_A)), .gwe(gwe), .rst(rst), .out(r1));
+   Nbit_reg #(n) r_2(.in(data2), .clk(clk), .we((i_rd_A == 3'b010 || i_rd_B == 3'b010) & (i_rd_we_A || i_rd_we_A)), .gwe(gwe), .rst(rst), .out(r2));
+   Nbit_reg #(n) r_3(.in(data3), .clk(clk), .we((i_rd_A == 3'b011 || i_rd_B == 3'b011) & (i_rd_we_A || i_rd_we_A)), .gwe(gwe), .rst(rst), .out(r3));
+   Nbit_reg #(n) r_4(.in(data4), .clk(clk), .we((i_rd_A == 3'b100 || i_rd_B == 3'b100) & (i_rd_we_A || i_rd_we_A)), .gwe(gwe), .rst(rst), .out(r4));
+   Nbit_reg #(n) r_5(.in(data5), .clk(clk), .we((i_rd_A == 3'b101 || i_rd_B == 3'b101) & (i_rd_we_A || i_rd_we_A)), .gwe(gwe), .rst(rst), .out(r5));
+   Nbit_reg #(n) r_6(.in(data6), .clk(clk), .we((i_rd_A == 3'b110 || i_rd_B == 3'b110) & (i_rd_we_A || i_rd_we_A)), .gwe(gwe), .rst(rst), .out(r6));
+   Nbit_reg #(n) r_7(.in(data7), .clk(clk), .we((i_rd_A == 3'b111 || i_rd_B == 3'b111) & (i_rd_we_A || i_rd_we_A)), .gwe(gwe), .rst(rst), .out(r7));
    
-   assign o_rs_data = i_rs == 3'b000 ? r0 : (i_rs == 3'b001 ? r1 :  (i_rs == 3'b010 ? r2 : (i_rs == 3'b011 ? r3 : (i_rs == 3'b100 ? r4 : (i_rs == 3'b101 ? r5 : (i_rs == 3'b110 ? r6 : r7))))));
-   assign o_rt_data = i_rt == 3'b000 ? r0 : (i_rt == 3'b001 ? r1 :  (i_rt == 3'b010 ? r2 : (i_rt == 3'b011 ? r3 : (i_rt == 3'b100 ? r4 : (i_rt == 3'b101 ? r5 : (i_rt == 3'b110 ? r6 : r7))))));
+   assign o_rs_data_A = i_rs_A == 3'b000 ? r0_A : (i_rs_A == 3'b001 ? r1_A :  (i_rs_A == 3'b010 ? r2_A : (i_rs_A == 3'b011 ? r3_A : (i_rs_A == 3'b100 ? r4_A : (i_rs_A == 3'b101 ? r5_A : (i_rs_A == 3'b110 ? r6_A : r7_A))))));
+   assign o_rt_data_A = i_rt_A == 3'b000 ? r0_A : (i_rt_A == 3'b001 ? r1_A :  (i_rt_A == 3'b010 ? r2_A : (i_rt_A == 3'b011 ? r3_A : (i_rt_A == 3'b100 ? r4_A : (i_rt_A == 3'b101 ? r5_A : (i_rt_A == 3'b110 ? r6_A : r7_A))))));
+   
+   assign o_rs_data_B = i_rs_B == 3'b000 ? r0_B : (i_rs_A == 3'b001 ? r1_B :  (i_rs_B == 3'b010 ? r2_B : (i_rs_B == 3'b011 ? r3_B : (i_rs_B == 3'b100 ? r4_B : (i_rs_B == 3'b101 ? r5_B : (i_rs_B == 3'b110 ? r6_B : r7_B))))));
+   assign o_rt_data_B = i_rt_B == 3'b000 ? r0_B : (i_rt_A == 3'b001 ? r1_B :  (i_rt_B == 3'b010 ? r2_B : (i_rt_B == 3'b011 ? r3_B : (i_rt_B == 3'b100 ? r4_B : (i_rt_B == 3'b101 ? r5_B : (i_rt_B == 3'b110 ? r6_B : r7_B))))));
    
 
 endmodule
